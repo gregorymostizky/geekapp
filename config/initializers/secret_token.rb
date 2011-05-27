d4846521a81a4bcc182cbfaf8911bae2f1bf23e8
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-05-27 16:46:03 +0300

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geekapp::Application.config.secret_token = '6dfbca82d314975dfc781a3ad4ab99cf1f7c8f31033725be016936734ad77650abbbac631c5cc8979985b52761c7e8954442f3c9340bec5468b8466683baca74'
