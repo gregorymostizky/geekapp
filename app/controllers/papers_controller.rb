8c8c5efe9a45c32e45f059c6d7667ff1f76d3f74
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-05-28 22:17:37 +0300

    paper create form

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -6,4 +6,11 @@ class PapersController < ApplicationController
   def new
     @paper = Paper.new(:user => current_user)
   end
+
+  def create
+    @paper = Paper.new(params[:paper])
+    @paper.user = current_user
+    @paper.save
+    redirect_to :controller => :dashboard, :action => :index
+  end
 end
