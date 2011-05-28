b64383d61473fd067c5d2ee3284b2221a6af5187
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-05-28 21:29:25 +0300

    disable devise

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,17 +1,14 @@
 class FilesController < ApplicationController
   def index
+    puts 'hahah'
     @files = UserFile.all(:conditions => {:user_id => current_user })
   end
 
   def create
-=begin
     Rails.logger.info {current_user.inspect}
     @file = UserFile.create( :file => params[:file], :user => current_user )
     @files = [@file]
     Rails.logger.info {current_user.inspect}
     render "index"
-  rescue e
-    Rails.logger.info {e.inspect}
-=end
   end
 end
