32b32fc3cec3a41694af8143830eca18c586f78a
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-05-28 20:57:50 +0300

    current user

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -4,6 +4,7 @@ class FilesController < ApplicationController
   end
 
   def create
+    p current_user
     @file = UserFile.create( :file => params[:file], :user => current_user )
     @files = [@file]
     p current_user
